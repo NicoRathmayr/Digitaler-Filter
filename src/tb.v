e53227a7fd35da0c0ae043c3bd8e4f5d9ebe027f
NicoRathmayr <nico@rathmayr.cc>
2023-12-21 10:19:50 +0100

    try

--- src/tb.v
+++ src/tb.v
@@ -12,24 +12,25 @@ module tb;
     // gjfu
     // wire up the inputs and outputs
     reg  clk_i = 1'b1;
-    reg  rst_n_i = 1'b1;
+    reg  reset_i = 1'b1;
     reg  [7:0] x_i;
     wire [7:0] y_o;
     
 
-    tt_um_digitaler_filter tt_um_digitaler_filter(
+    tt_um_digitaler_filter 
+filter(
     // include power ports for the Gate Level test
-    `ifdef GL_TEST
-        .VPWR( 1'b1),
-        .VGND( 1'b0),
-    `endif
-        .x      (x_i),    // Dedicated inputs
-        .y     (y_o),   // Dedicated outputs
+    //`ifdef GL_TEST
+     //   .VPWR( 1'b1),
+       // .VGND( 1'b0),
+    //`endif
+        .ui_in      (x_i),    // Dedicated inputs
+        .uo_out     (y_o),   // Dedicated outputs
         .clk        (clk_i),      // clock
-        .rst_n      (rst_n_i)     // not reset
+        .rst_n      (reset_i)     // not reset
         );
 
-    always #5 clk_i = ~clk_i;
+    always #1 clk_i = ~clk_i;
 
     // this part dumps the trace to a vcd file that can be viewed with GTKWave
     initial begin
@@ -37,13 +38,12 @@ module tb;
         $dumpvars (0, tb);
         #1;
 	
-	#10 rst_n_i = 0;
-	#1 x_i = 8'h01;
-	#1 x_i = 8'h02;
-	#1 x_i = 8'h03;
-	#1 x_i = 8'h04;
+	#1 x_i = 8'h00;
+	#20 reset_i = 0;
+	#30 x_i = 8'hFF;
+	#1 x_i = 8'h00;
 
-	#100 $finish;
+	#70 $finish;
     end
 
 endmodule
--- src/tt_um_digitaler_filter.v
+++ src/tt_um_digitaler_filter.v
@@ -34,22 +34,26 @@ module tt_um_digitaler_filter(
     reg [7:0] x_reg [3:0];
     reg [15:0] product;
     reg [23:0] sum;
-    integer i;
+    //integer i;
     //assign y = (rst_n) ? 8'h00 : sum[15:8];
     always @(posedge clk or posedge reset) begin
 	//h <= {8'h06, 8'h1C, 8'h1C, 8'h06};
 	//for (i=0;i<1;i=i+1) begin
-            h[0] <= 8'h06;
-	    h[1] <= 8'h1C;
-	    h[2] <= 8'h1C;
-	    h[3] <= 8'h06;
+            h[0] <= 8'h3C;
+	    h[1] <= 8'h44;
+	    h[2] <= 8'h44;
+	    h[3] <= 8'h3C;
 	//end
-    	if (reset) begin
+    	if (!reset) begin
             sum <= 24'h000000;
 	    product <= 16'h0000;
-	    for (i=0;i<4;i=i+1) begin
-            	x_reg[i] <= 8'h00;
-	    end
+	    x_reg[0] <= 8'h00;
+	    x_reg[1] <= 8'h00;
+	    x_reg[2] <= 8'h00;
+	    x_reg[3] <= 8'h00;
+	 //   for (i=0;i<4;i=i+1) begin
+         //   	x_reg[i] <= 8'h00;
+	 //   end
         end else begin
             x_reg[0] <= x;
 	    x_reg[1] <= x_reg[0];
@@ -57,7 +61,7 @@ module tt_um_digitaler_filter(
 	    x_reg[3] <= x_reg[2];
 
 	    product <= h[0] * x_reg[0] + h[1] * x_reg[1] + h[2] * x_reg[2] + h[3] * x_reg[3];
-	    //product <= 16'h0000;
+	    //product <= x_reg[0];
 	    sum <= sum + {8'b00000000,product};
 	    //sum <= sum + {{8{1'b0}},product};
 	    //sum <= $signed(sum) + $signed({8'b00000000, product});
@@ -65,5 +69,5 @@ module tt_um_digitaler_filter(
 	    
 	end
    end
-   assign y = (reset) ? 8'h00 : sum[15:8];
+   assign y = (!reset) ? 8'h00 : sum[15:8];
 endmodule
