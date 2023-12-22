62152f63a556d5523dfe1fd5429e700cfbfb5468
NicoRathmayr <nico@rathmayr.cc>
2023-12-22 10:56:37 +0100

    add_input5

--- src/tt_um_digitaler_filter.v
+++ src/tt_um_digitaler_filter.v
@@ -29,8 +29,8 @@ module tt_um_digitaler_filter(
     assign uo_out[7:0] = y;
 
     reg [1:0] counter;
-    reg [7:0] h [2:0];
-    reg [7:0] x_reg [2:0];
+    reg [7:0] h [1:0];
+    reg [7:0] x_reg [1:0];
     reg [15:0] product;
     reg [23:0] sum;
     reg h_start;
@@ -41,13 +41,13 @@ module tt_um_digitaler_filter(
 	    counter <= 2'b00;
 	    h[0] <= 8'h00;
 	    h[1] <= 8'h00;
-	    h[2] <= 8'h00;
+	    //h[2] <= 8'h00;
 	    //h[3] <= 8'h00;
             sum <= 24'h000000;
 	    product <= 16'h0000;
 	    x_reg[0] <= 8'h00;
 	    x_reg[1] <= 8'h00;
-	    x_reg[2] <= 8'h00;
+	    //x_reg[2] <= 8'h00;
 	    //x_reg[3] <= 8'h00;
 	    h_start <= 1'b1;
         end else begin
@@ -59,12 +59,12 @@ module tt_um_digitaler_filter(
 		2'b01:
 		    h[1] <= const_h;
 		2'b10:
-		    h[2] <= const_h;
+		    h[1] <= const_h;
 		2'b11:
-		    h[2] <= const_h;
+		    h[1] <= const_h;
 	    endcase
 
-	    if (counter < 2'b10)
+	    if (counter < 2'b01)
 		counter <= counter + 1;
 	    else begin
 		counter <= 2'b00;
@@ -73,10 +73,10 @@ module tt_um_digitaler_filter(
 	    end
             x_reg[0] <= x;
 	    x_reg[1] <= x_reg[0];
-	    x_reg[2] <= x_reg[1];
+	    //x_reg[2] <= x_reg[1];
 	    //x_reg[3] <= x_reg[2];
 
-	    product <= h[0] * x_reg[0] + h[1] * x_reg[1] + h[2] * x_reg[2]; //+ h[3] * x_reg[3];
+	    product <= h[0] * x_reg[0] + h[1] * x_reg[1]; //+ h[2] * x_reg[2]; //+ h[3] * x_reg[3];
 	    sum <= sum + {8'b00000000,product};
 
 	end
